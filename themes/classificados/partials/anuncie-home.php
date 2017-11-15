659f7c352d3d870dcce508da27916fe52170adde
RenatoCabral <carvalho.renato6@gmail.com>
2017-11-14 23:22:35 -0200

    incluso icone consulta fipe e anuncie, alinhado textos

--- themes/classificados/partials/anuncie-home.php
+++ themes/classificados/partials/anuncie-home.php
@@ -1,12 +1,15 @@
 <div class="col s12 m6 l3"><br><br><br>
     <div class="card-panel z-depth-1 card-anuncie-home">
         <div class="card-content">
-            <a href=""><img src="icone-vender-carro.png" alt=""></a>
-            <h5  class="card-title">Venda seu veículo</h5>
-            <p>Anuncie no ClassiCarros e seu carro será visto por pessoas de todo o Brasil.</p>
-            <p>Anuncio seguro e venda rápido!</p>
+            <img class="img-responsive icon-finance-fipe-home" src="<?= get_bloginfo('template_url') ?>/img/icon-sale-car-64.png">
+            <h5  class="card-title content-sale-car-home">Venda seu veículo</h5>
+            <p class="content-sale-car-home">Anuncie no ClassiCarros e seu carro será visto por pessoas de todo o Brasil.</p>
+            <p class="content-sale-car-home">Anuncio seguro e venda rápido!</p>
         </div>
-        <a class="waves-effect waves-light btn">Anuncie Agora</a>
+        <a class="waves-effect waves-light btn btn-sale-car-home">Anuncie Agora</a>
+<!--        <a class="menu-my-account transition-300ms waves-effect waves-light btn" target="_blank" href="--><?//= get_permalink( get_page_by_path( 'minha-conta' ) ) ?><!--">Anuncie Agora</a>-->
+
+
 
         <!--        <div class="card-action">-->
 <!--            <a href="#">This is a link</a>-->
--- themes/classificados/partials/fipe-home.php
+++ themes/classificados/partials/fipe-home.php
@@ -1,16 +1,16 @@
-<div class="col s12 m12 l2 fipe">
-        <h5 class="title_fipe">Consulta FIPE</h5>
-
-        <div class="row fipe_icons">
-            <i class="material-icons medium">drive_eta</i>
-            <i class="material-icons medium">drive_eta</i>
+<div class="col s12 m6 l3"><br><br><br>
+    <div class="card-panel z-depth-1 card-anuncie-home">
+        <div class="card-content">
+            <img class="img-responsive icon-finance-fipe-home" src="<?= get_bloginfo('template_url') ?>/img/icon-finance-64.png">
+            <h5 class="card-title content-fipe-home">Tabela FIPE</h5>
+            <p class="content-fipe-home">Veja o preço do veículo que você deseja comprar ou vender!</p>
         </div>
-
-        <p>Comprar ou vender, mas não sabe o preço?<br>
-            Consulte na tabela <strong>FIPE</strong>!</p>
-        <div class="row fipe_button">
-            <a target="_blank" href="http://veiculos.fipe.org.br/" class="btn waves-effect waves-light" >
-                Consultar
-            </a>
-        </div>
+        <a class="waves-effect waves-light btn btn-consulta-fipe-card" href="http://veiculos.fipe.org.br/" target="_blank"">Consulte agora</a>
+        <!--        <div class="card-action">-->
+        <!--            <a href="#">Contato</a>-->
+        <!--            <a href="#">Dúvidas frequentes</a>-->
+        <!--        </div>-->
+    </div>
 </div>
+
+
